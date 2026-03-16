f3e3088f44229154edefe189346d47ad91ada5a3
gooop <gcastaneda456@gmail.com>
2026-03-15 20:26:12 -0600

    Add logging for ping command

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,5 +1,8 @@
 import { GatewayDispatchEvents } from '@discordjs/core';
 import { createEventHandler } from './eventHandler';
+import { createLogger } from '../logger';
+
+const log = createLogger('messageCreate');
 
 export const messageCreateHandler = createEventHandler(
     GatewayDispatchEvents.MessageCreate,
@@ -9,6 +12,7 @@ export const messageCreateHandler = createEventHandler(
         }
 
         if (data.content === '!ping') {
+            log.info(`User @${data.author.username}#${data.author.discriminator} called !ping`);
             await api.channels.createMessage(data.channel_id, {
                 content: 'pong!',
                 message_reference: { message_id: data.id },
